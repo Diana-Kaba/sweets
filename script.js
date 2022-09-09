42176ecef137a4a983988031da09ce3a05473345
Diana <kabadiana327@gmail.com>
2022-09-09 18:16:54 +0300

    sweets

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 "use strict";
-let count = prompt('Сколько у Вас есть конфет?');
-while() {
+let count = prompt("Сколько у Вас есть конфет в коробке?");
+while (count > 0) {
+  count--;
+  let q = confirm("Еще конфету?");
+  if (q == true) {
     count--;
-    let q = confirm('Еще конфету?');
-    if(q == true) {
-        count--;
-    } else {
-        break;
-    }
+    alert("Была съедена еще одна конфета.");
+  } else {
+    break;
+  }
+  if (count === 0) {
+    alert("Пора покупать другую коробку…");
+  }
 }
