caa5a251e80e96255c6b69fccd047d22d92bd341
Diana <kabadiana327@gmail.com>
2022-09-10 14:04:52 +0300

    exercise for 12

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 "use strict";
 let count = prompt("Сколько у Вас есть конфет в коробке?");
+let n = count / 2;
 while (count > 0) {
-  count--;
   let q = confirm("Еще конфету?");
-  if (q == true) {
+  if (q) {
     count--;
     alert("Была съедена еще одна конфета.");
   } else {
     break;
   }
-  if (count === 0) {
+  if (q > n) {
+    alert("Конфет еще много.");
+  } else if (q < n && q > 3) {
+    alert("Конфет маловато...");
+  } else if (q < n && q < 3) {
+    alert("Конфеты заканчиваются!");
+  }
+  if (count == 0) {
     alert("Пора покупать другую коробку…");
   }
 }
